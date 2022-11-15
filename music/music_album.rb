9aa1419b49a24f01ccc77fbb9a73bf62c7de4982
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-11-15 23:11:36 +0530

    Added base files

---
+++ music/music_album.rb
@@ -0,0 +1,16 @@
+require_relative '../item'
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify, :name, :album_title, :artist
+
+  def initialize(album_title, artist, on_spotify, id: nil, publish_date: nil)
+    super(id, publish_date)
+    @album_title = album_title
+    @artist = artist
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && @on_spotify
+  end
+end
