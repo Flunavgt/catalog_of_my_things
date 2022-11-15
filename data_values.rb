b7077f959b6f2d31f1d0e1bf5b19cff6bfe9373f
Federico Luna <fluna1973@yahoo.com>
2022-11-15 09:15:20 -0600

    music and basic ui

---
+++ data_values.rb
@@ -0,0 +1,24 @@
+module DataValues
+  @albums = []
+  @genres = []
+  @authors = []
+  @labels = []
+  @artist = []
+  @album_title = []
+
+  def self.albums
+    @albums
+  end
+
+  def self.genres
+    @genres
+  end
+
+  def self.album_title
+    @album_title
+  end
+
+  def self.artist
+    @artist
+  end
+end
---
+++ main.rb
@@ -0,0 +1,9 @@
+require './app'
+
+def main
+  app = App.new
+  puts 'Welcome to Catalog of things!'
+  app.run
+end
+
+main
