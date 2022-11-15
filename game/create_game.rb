22e25f20cab92fbba1f9f494cbf9b7ccbc9633ba
addis <betsega23@gmail.com>
2022-11-16 01:18:56 +0300

    Add game class

---
+++ game/create_game.rb
@@ -0,0 +1,27 @@
+class CreateGame
+  @user_answer = ''
+  def self.switch_case(ans)
+    case ans
+    when 'y'
+      true
+    when 'n'
+      false
+    else
+      puts "Please Enter Y/y for 'YES' and N/n for 'No' \n Answer: "
+      answer = gets.chomp.downcase
+      switch_case(answer)
+    end
+  end
+
+  def self.create_game(games)
+    puts 'Is the game multiplayer? [Y | N]'
+    print "\nAnswer: "
+    user_answer = gets.chomp.downcase
+    puts 'last played at'
+    print "\nAnswer: "
+    last_played = gets.chomp
+    game = Game.new(switch_case(user_answer), last_played)
+    games << game
+    puts "\nGame created successfully"
+  end
+end
---
+++ game/game.rb
@@ -0,0 +1,18 @@
+require 'date'
+require_relative '../item'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played
+
+  def initialize(multiplayer, last_played, id: nil, publish_date: nil)
+    super(id, publish_date)
+    @multiplayer = multiplayer
+    @last_played = last_played
+  end
+
+  private
+
+  def can_be_archived?
+    super && DateTime.now.strftime('%Y').to_i - @last_played[6..10].to_i > 2
+  end
+end
