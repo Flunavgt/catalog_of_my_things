ea7f11ab193205378b5129a841d086248737d821
addis <betsega23@gmail.com>
2022-11-17 01:59:55 +0300

    Remove duplicate Gemfile

--- schema/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE book (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    publish_date DATE,
-    archived BOOLEAN,
-    publisher VARCHAR(100),
-    cover_state VARCHAR(50),
-    label_id INTEGER REFERENCES label (id)
-);
